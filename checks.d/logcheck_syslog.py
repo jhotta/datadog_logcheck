104d068463ceabc5037732072ed20f6480691c03
Naotaka Jay HOTTA <fukuzo@cybertron.co.jp>
2016-07-21 13:16:44 +0900

    writing my first check

--- checks.d/logcheck_syslog.py
+++ checks.d/logcheck_syslog.py
@@ -1,112 +1,62 @@
-"""
-logcheck_syslog.py - Syslog parser and checker for Datadog Agent
+import time
+import requests
 
-Datadog implementation of logcheck.  You can send unknown logs into Datadog.
-
-Datadog: https://www.datadoghq.com/
-logcheck: http://logcheck.alioth.debian.org/
-
-Usage:
-    1. Add dd-agent user into adm group to read syslog
-       adduser dd-agent adm
-    2. Add dd-agent user into logcheck group to read logcheck ignore rules
-       adduser dd-agent logcheck
-    3. Write as follows into your datadog.conf
-       dogstreams: /var/log/syslog:/path/to/logcheck_syslog.py:logcheck
-    4. Restart datadog-agent
-       service datadog-agent restart
-"""
-
-import re
-import socket
-from datetime import datetime
-import glob
 from checks import AgentCheck
+from hashlib import md5
 
-
-# Constants
-EVENT_TYPE = "logcheck.syslog"
-EVENT_TITLE = "System Events on {}".format(socket.getfqdn())
-LINE_REGEXP = re.compile(r'^(\w{3} \d+? [\d:]+?) \S+? (.*)$')
-
-
-class HelloCheck(AgentCheck):
+class HTTPCheck(AgentCheck):
     def check(self, instance):
-        self.gauge('hello.japan', 1)
+        if 'url' not in instance:
+            self.log.info("Skipping instance, no url found.")
+            return
 
+        # Load values from the instance config
+        url = instance['url']
+        default_timeout = self.init_config.get('default_timeout', 5)
+        timeout = float(instance.get('timeout', default_timeout))
 
-# Load logcheck ignore rules
-rules = set()
-for file in glob.iglob("/etc/logcheck/ignore.d.server/*"):
-    with open(file) as f:
-        for regexp in f:
-            regexp = regexp.rstrip()
-            if len(regexp) == 0:
-                break
+        # Use a hash of the URL as an aggregation key
+        aggregation_key = md5(url).hexdigest()
 
-            # Replace POSIX character class
-            regexp = regexp.replace("[:alnum:]", r"0-9A-Za-z")
-            regexp = regexp.replace("[:alpha:]", r"A-Za-z")
-            regexp = regexp.replace("[:blank:]", r"\s")
-            regexp = regexp.replace("[:digit:]", r"\d")
-            regexp = regexp.replace("[:graph:]", r"!-~")
-            regexp = regexp.replace("[:lower:]", r"a-z")
-            regexp = regexp.replace("[:print:]", r"!-~ ")
-            regexp = regexp.replace("[:punct:]", r"!\"#$%&'()*+,./:;<=>?@\[\\\]^`{|}~")
-            regexp = regexp.replace("[:space:]", r"\s")
-            regexp = regexp.replace("[:upper:]", r"A-Z")
-            regexp = regexp.replace("[:xdigit:]", r"0-9A-Fa-f")
-
-            rules.add(re.compile(regexp))
-
-
-def logcheck(logger, line):
-    if len(line) == 0:
-        return None
-
-    logger.debug(line)
-    for rule in rules:
-        if rule.search(line):
-            return None
-
-    # Prepare event object
-    event = {
-        "event_type": EVENT_TYPE,
-        "msg_title": EVENT_TITLE,
-        "msg_text": line,
-        "alert_type": "error",
-    }
-
-    # Parse line
-    try:
-        date_str, msg = LINE_REGEXP.match(line).groups()
-        event["msg_text"] = msg
+        # Check the URL
+        start_time = time.time()
         try:
-            event["timestamp"] = _parse_datetime(dt_str)
-        except Exception:
-            None
-    except:
-        None
+            r = requests.get(url, timeout=timeout)
+            end_time = time.time()
+        except requests.exceptions.Timeout as e:
+            # If there's a timeout
+            self.timeout_event(url, timeout, aggregation_key)
+            return
 
-    return [event]
+        if r.status_code != 200:
+            self.status_code_event(url, r, aggregation_key)
 
+        timing = end_time - start_time
+        self.gauge('http.reponse_time', timing, tags=['http_check'])
 
-def _parse_datetime(dt_str):
-    dt = datetime.strptime(dt_str, "%b %d %H:%M:%S")
+    def timeout_event(self, url, timeout, aggregation_key):
+        self.event({
+            'timestamp': int(time.time()),
+            'event_type': 'http_check',
+            'msg_title': 'URL timeout',
+            'msg_text': '%s timed out after %s seconds.' % (url, timeout),
+            'aggregation_key': aggregation_key
+        })
 
-    # Set correct year value
-    if (dt.month > datetime.now().month):
-        dt.replace(datetime.now().year - 1)
-    else:
-        dt.replace(datetime.now().year)
-
-    # Return unix timestamp
-    return int(dt.strftime('%s'))
-
-
-# Test is not implemented yet
-def test():
-    return True
+    def status_code_event(self, url, r, aggregation_key):
+        self.event({
+            'timestamp': int(time.time()),
+            'event_type': 'http_check',
+            'msg_title': 'Invalid reponse code for %s' % url,
+            'msg_text': '%s returned a status of %s' % (url, r.status_code),
+            'aggregation_key': aggregation_key
+        })
 
 if __name__ == '__main__':
-    test()
+    check, instances = HTTPCheck.from_yaml('/path/to/conf.d/http.yaml')
+    for instance in instances:
+        print "\nRunning the check against url: %s" % (instance['url'])
+        check.check(instance)
+        if check.has_events():
+            print 'Events: %s' % (check.get_events())
+        print 'Metrics: %s' % (check.get_metrics())
